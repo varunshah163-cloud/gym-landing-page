d248f2a1ef3a3455a330fb15037d5c3627b55740
varunshah163-cloud <varunshah163@gmail.com>
2026-04-08 11:04:01 +0530

    added success.html

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const elements = document.querySelectorAll(".fade-in");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    const top = section.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight - 100;
+
+    if (top < triggerPoint) {
+      section.classList.add("show");
+    }
+  });
+});
+
+//Form submit alert
 document.querySelector("form").addEventListener("submit", function () {
-  alert("Message sent successfully!");
+  alert("Sending...");
 });
